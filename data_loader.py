d08579940bdeaeeb182eabe39a3985364a0ed6f9
GiaSoon2000 <116819331+GiaSoon2000@users.noreply.github.com>
2024-08-12 17:49:01 +0800

    Add files via upload

--- data_loader.py
+++ data_loader.py
@@ -2,14 +2,15 @@
 
 import pandas as pd
 
-def load_data(file_path):
-        try:
-            data = pd.read_csv('Student Mental health.csv')
-            print("Data loaded successfully!")
-            return data
-        except FileNotFoundError:
-            print("File not found. Please check the file path.")
-        except pd.errors.EmptyDataError:
-            print("No data found. The file is empty.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+def load_data():
+    try:
+        data = pd.read_csv('Student Mental health.csv')
+        print("Data loaded successfully!")
+        return data
+        
+    except FileNotFoundError:
+        print("File not found. Please check the file path.")
+    except pd.errors.EmptyDataError:
+        print("No data found. The file is empty.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
